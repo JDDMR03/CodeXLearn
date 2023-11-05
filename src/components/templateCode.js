8f6fa3b194e67fec777a455f5f70f3273eb1844a
Juan Moreno <dios.morenor@uanl.edu.mx>
2023-11-05 02:03:57 -0600

    adding copy to clipboard feature

--- src/components/templateCode.js
+++ src/components/templateCode.js
@@ -1,5 +1,7 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import styles from '@/styles/Article.module.css'
 
 export default function CodeViewer() {
     const code = `
@@ -14,8 +16,13 @@ export default function CodeViewer() {
     `;
 
     return (
-      <SyntaxHighlighter language="cpp" style={solarizedlight} showLineNumbers={true}>
-        {code}
-      </SyntaxHighlighter>
+      <div className={styles.code}> 
+        <SyntaxHighlighter language="cpp" style={solarizedlight} showLineNumbers={true}>
+          {code}
+        </SyntaxHighlighter>
+        <CopyToClipboard text={code}>
+          <button><i class="nf nf-md-content_copy"></i></button>
+        </CopyToClipboard>
+      </div>
     );
 }
